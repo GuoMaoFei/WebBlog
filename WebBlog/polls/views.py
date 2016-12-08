6deb916274ea27397d9c11077a24add0566c2395
GuoMaoFei <1025006407@qq.com>
2016-12-08 17:17:03 +0800

    view

--- WebBlog/polls/views.py
+++ WebBlog/polls/views.py
@@ -1,3 +1,9 @@
+# encoding: utf-8
 from django.shortcuts import render
-
+from .models import Postinfo
 # Create your views here.
+#主页面
+def index(request):
+    list=postinfo.objects.order_by('-pubdate')[:5]
+    context={'list':list}
+    return render(request,'polls/blog_index.html',context)
